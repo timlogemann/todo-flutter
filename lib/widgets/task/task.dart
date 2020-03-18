f3f1f647a87811510dc854a7940380eaa13655dc
Tim Logemann <tim@logemann.nu>
2020-03-18 17:38:07 +0100

    add hero to task widget

--- lib/widgets/task/task.dart
+++ lib/widgets/task/task.dart
@@ -12,50 +12,60 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Row(children: <Widget>[
-      Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              task.todo,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
-            margin: EdgeInsets.only(left: 5),
-          )
-        ],
+    return GestureDetector(
+      onTap: onPressed,
+      child: Hero(
+        transitionOnUserGestures: true,
+        tag: 'taskHero' + task.id.toString(),
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      task.todo,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
+                    margin: EdgeInsets.only(left: 5),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      new DateFormat('dd-MM-yyyy').format(task.dueDate),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      task.project,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
+                  ),
+                ],
+              ),
+              // Column(
+              //   children: <Widget>[
+              //     Container(
+              //       child: Text(
+              //         task.note,
+              //       ),
+              //       padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
+              //       margin: EdgeInsets.only(right: 5),
+              //     ),
+              //   ],
+              // ),
+            ],
+          ),
+        ),
       ),
-      Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              new DateFormat('dd-MM-yyyy').format(task.dueDate),
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
-          )
-        ],
-      ),
-      Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              task.project,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
-          )
-        ],
-      ),
-      Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              task.note,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12),
-            margin: EdgeInsets.only(right: 5),
-          )
-        ],
-      ),
-    ]));
+    );
   }
 }
