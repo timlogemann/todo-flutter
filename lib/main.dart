90031b9b719629dc5f737f6b5a76ef3f69701180
Tim Logemann <tim@logemann.nu>
2020-03-18 10:17:46 +0100

    turn main.dart into a router

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todone/blocs/task/task_bloc.dart';
 import 'package:todone/blocs/task/task_index.dart';
-import 'package:todone/widgets/index.dart';
-import 'package:intl/intl.dart';
-// import 'package:todone/widgets/index.dart';
+import 'package:todone/routes/index.dart';
 
 void main() => runApp(BlocProvider<TaskBloc>(
       create: (context) => TaskBloc(),
@@ -27,133 +25,10 @@ class MyApp extends StatelessWidget {
         accentColor: Color.fromARGB(255, 54, 63, 71),
         bottomAppBarColor: Color.fromARGB(255, 26, 30, 34),
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Dashboard'),
+      routes: <String, WidgetBuilder>{
+        '/create': (BuildContext context) => FormRoute(title: 'Create Task'),
+      },
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  TaskBloc _taskBloc;
-
-  _HomePageState();
-
-  @override
-  void initState() {
-    _taskBloc = BlocProvider.of<TaskBloc>(context);
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print(_taskBloc.state);
-
-    return Scaffold(
-      extendBody: true,
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.fromLTRB(10, 40, 10, 0),
-                color: Theme.of(context).backgroundColor,
-                child: BlocBuilder<TaskBloc, TaskState>(
-                    builder: (BuildContext context, TaskState state) {
-                  if (state is TasksLoading) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  return ListView.separated(
-                    itemBuilder: (task, index) =>
-                        Task(onPressed: null, task: state.tasks[index]),
-                    separatorBuilder: (_, __) => Divider(),
-                    itemCount: state.tasks.length,
-                  );
-                }),
-              ),
-              flex: 3,
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Theme.of(context).bottomAppBarColor,
-        elevation: 5,
-        child: Container(
-          height: 60,
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                iconSize: 30.0,
-                padding: EdgeInsets.only(left: 28.0),
-                icon: Icon(Icons.list),
-                color: Theme.of(context).accentColor,
-                onPressed: () {},
-              ),
-              // IconButton(
-              //   iconSize: 30.0,
-              //   padding: EdgeInsets.only(right: 28.0),
-              //   icon: Icon(Icons.search),
-              //   onPressed: () {},
-              // ),
-              // IconButton(
-              //   iconSize: 30.0,
-              //   padding:  EdgeInsets.only(left: 28.0),
-              //   icon: Icon(Icons.notifications),
-              //   onPressed: () {},
-              // ),
-              IconButton(
-                iconSize: 30.0,
-                padding: EdgeInsets.only(right: 28.0),
-                icon: Icon(Icons.search),
-                color: Theme.of(context).accentColor,
-                onPressed: () {},
-              )
-            ],
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton.extended(
-        label: Text('Add task'),
-        icon: Icon(
-          Icons.edit,
-          size: 16,
-        ),
-        onPressed: () {},
-        tooltip: 'Increment',
-        elevation: 2.0,
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
